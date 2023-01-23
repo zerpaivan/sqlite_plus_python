1ec98d173bd4ad6971df4097c6368ade0f41e00b
zerpaian <zerpaivan@gmail.com>
2023-01-22 23:01:46 -0400

    update 22012023

--- python_sqlite.py
+++ python_sqlite.py
@@ -1,42 +1,70 @@
-'''Script para trabajar con Sqlite desde python'''
+"""Script para trabajar con Sqlite desde python"""
 # conectar/crear base de datos en python
 import sqlite3
-conn = sqlite3.connect('mi_database.db')
+
+conn = sqlite3.connect("mi_database.db")
 
 # trabajar con una base de datos desde la memoria
 # conn=sqlite3.connect('mi_database.db')
 
 # Crear un Tabla de base de datos
-cursor = conn.cursor() # creacion del Objeto cursor
+cursor = conn.cursor()  # creacion del Objeto cursor
 
 # creacion de la tabla
-# cursor.execute(
-#     '''CREATE TABLE CUSTOMERS(
-#         first_name TEXT,
-#         last_name TEXT,
-#         email TEXT
+cursor.execute(
+    '''CREATE TABLE IF NOT EXISTS CUSTOMERS(
+        ID INTEGER PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        email TEXT
 
-#     )'''
-# )
+    )'''
+)
 
 # Insertar registros en la tabla
-# cursor.execute("INSERT INTO CUSTOMERS VALUES ('Ivan', 'Zerpa', 'zerpaivan@gmail.com')")
+# cursor.execute("INSERT INTO CUSTOMERS VALUES (1, 'Ivan', 'Zerpa', 'zerpaivan@gmail.com')")
 
 # insertar multiple registros en una tabla.
 many_customers = [
-    ('Maria', 'Perez', 'perezm77@gmail.com'),
-    ('Pedro', 'Hernadez', 'H_pedrp@outlook.com'),
-    ('Ana','Meza','mezana@gmail.com')
-    ]
+    (2, "Maria", "Perez", "perezm77@gmail.com"),
+    (3, "Pedro", "Hernadez", "H_pedrp@outlook.com"),
+    (4, "Ana", "Meza", "mezana@gmail.com"),
+]
+#  usar el metodo executemany para multiples registros
+# cursor.executemany("INSERT INTO CUSTOMERS VALUES (?,?,?,?)", many_customers)
 
-# cursor.executemany("INSERT INTO CUSTOMERS VALUES (?,?,?)", many_customers)
-
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # Query a Fetchall
-cursor.execute("SELECT * FROM CUSTOMERS") # esta es la consulta
-print(cursor.fetchall()) # imprime los valores de la consulta
+# cursor.execute("SELECT * FROM CUSTOMERS WHERE email LIKE '%gmail.com'")  # esta es la consulta
 
-conn.commit() # como un" push hacia la base de datos
+# fetchall transforma todos los registros la consulta en un array.
+# print(cursor.fetchall())  # imprime los valores de la consulta (array)
+
+# array de la primera sila de la tabla
+# cursor.fetchone() 
+
+# array de las filas indicadas
+# cursor.fetchmany(3)
+
+#------------------------------------------------------------------------------
+# actualiza un registro
+# UPDATE la_tabla SET la_columna = nuevo_valor WHERE condicion
+# cursor.execute("UPDATE CUSTOMERS SET first_name = 'Tana' WHERE last_name = 'Meza'")
+# -----------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Borrar un registro
+# cursor.execute("DELETE FROM CUSTOMERS WHERE last_name = 'Perez'")
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+#  ordenar una consulta
+cursor.execute("SELECT last_name, first_name FROM CUSTOMERS ORDER BY last_name DESC")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+conn.commit()  # como un" push hacia la base de datos
 
 # cerrar la coneccion
 conn.close()
