3236597ba9c2bf289df7c0de5cee35398cdfe03a
zerpaivan <zerpaivan@gmail.com>
2023-06-29 10:37:03 -0400

    update 29062023

--- Practice_problems_SQLITE/mis_soluciones_practice_problems.sql
+++ Practice_problems_SQLITE/mis_soluciones_practice_problems.sql
@@ -172,5 +172,119 @@ where OrderDate > (SELECT date(max(OrderDate), '-1 year') FROM Orders)
 GROUP BY ShipCountry
 ORDER BY OrderDate
 */
+--  29
+/*
+SELECT 
+Employees.EmployeeID,
+Employees.LastName,
+Orders.OrderID,
+Products.ProductName,
+'Order Details'.Quantity
+FROM Orders
+
+LEFT JOIN Employees
+    ON Employees.EmployeeID = Orders.EmployeeID
+LEFT JOIN 'Order Details'
+    ON Orders.OrderID = 'Order Details'.OrderID
+LEFT JOIN Products
+ON 'Order Details'.ProductID = Products.ProductID
+ORDER BY Orders.OrderID, Products.ProductID;
+*/
+
+-- 30
+/*
+SELECT Count(Orders.OrderID) AS count_orders, Customers.CustomerID FROM Customers
+LEFT JOIN Orders
+ON Orders.CustomerID = Customers.CustomerID
+GROUP BY Customers.CustomerID
+ORDER by count_orders;
+*/
+
+-- 31
+-- SELECT DISTINCT(CustomerID) FROM Orders;
+--SELECT DISTINCT(CustomerID) FROM Orders WHERE EmployeeID = 4;
+
+/*
+SELECT DISTINCT(Orders.CustomerID), prueba.CustomerID FROM Orders
+LEFT JOIN (SELECT DISTINCT(CustomerID) FROM Orders WHERE EmployeeID = 4) AS Prueba
+ON Orders.CustomerID = prueba.CustomerID
+WHERE prueba.CustomerID IS NULL;
+*/
+
 
 -- investigar: subconsulta y expresión de tabla común (CTE)
+
+-- Advanced Problems
+-- 32
+/*
+SELECT 
+    'Order Details'.OrderID, 
+    ORDERS.CustomerID, 
+    Customers.CompanyName, Orders.OrderDate, 
+    SUM('Order Details'.UnitPrice * 'Order Details'.Quantity) as Subtotal
+FROM 'Order Details'
+LEFT JOIN Orders
+    ON 'Order Details'.OrderID == Orders.OrderID
+LEFT JOIN Customers
+    ON Orders.CustomerID == Customers.CustomerID
+WHERE Orders.OrderDate > '2015-12-31' AND Orders.OrderDate <= '2016-12-31'
+GROUP BY 'Order Details'.OrderID
+HAVING Subtotal > 10000;
+*/
+
+-- Otra forma de resolver el problema
+/* 
+SELECT OrderID, Subtotal 
+FROM (
+    SELECT *, SUM(UnitPrice * Quantity) as Subtotal
+    FROM 'Order Details'
+    GROUP BY OrderID
+)
+WHERE Subtotal > 10000;
+*/
+
+-- 33
+/*
+SELECT 
+customers.CustomerID,
+SUM('Order Details'.Quantity * 'Order Details'.UnitPrice) AS TotalByCustomer  
+FROM Customers
+LEFT JOIN Orders
+ON Customers.CustomerID == Orders.CustomerID
+LEFT JOIN 'Order Details'
+ON Orders.OrderID == 'Order Details'.OrderID
+GROUP BY Customers.CustomerID;
+*/
+
+-- Total por Customers
+
+/*
+SELECT sum('Order Details'.Quantity*'Order Details'.UnitPrice) AS TotalBycustomer FROM Customers
+INNER JOIN Orders
+ON Customers.CustomerID == Orders.CustomerID
+INNER JOIN 'Order Details'
+ON Orders.OrderID == 'Order Details'.OrderID
+GROUP BY Customers.CustomerID;
+*/
+
+
+SELECT 
+    'Order Details'.OrderID, 
+    ORDERS.CustomerID, 
+    Customers.CompanyName, Orders.OrderDate, 
+    SUM('Order Details'.UnitPrice * 'Order Details'.Quantity) as TotalByOrder
+FROM 'Order Details'
+LEFT JOIN Orders
+    ON 'Order Details'.OrderID == Orders.OrderID
+LEFT JOIN Customers
+    ON Orders.CustomerID == Customers.CustomerID
+-- WHERE Orders.OrderDate > '2016-12-31' AND Orders.OrderDate <= '2018-12-31'
+GROUP BY 'Order Details'.OrderID, Customers.CustomerID
+HAVING TotalByOrder > 10000 AND 
+    (SELECT sum('Order Details'.Quantity*'Order Details'.UnitPrice) AS TotalBycustomer FROM Customers
+    INNER JOIN Orders
+    ON Customers.CustomerID == Orders.CustomerID
+    INNER JOIN 'Order Details'
+    ON Orders.OrderID == 'Order Details'.OrderID
+    GROUP BY Customers.CustomerID) > 10000
+ORDER BY Customers.CustomerID;
